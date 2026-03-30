72a7a29f8f927ff82fc8b3707b6bd69fea0a6064
Adrien Terrier <adrienterrier.pro@gmail.com>
2026-03-30 12:09:33 +0200

    Add files via upload

---
+++ Classe.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    class Classe
+    {
+        public string nomClasse { get; set; }
+        public List<Eleve> eleves { get; set; }
+        public List<string> matieres { get; set; }
+        public List<float> moyennesClasse { get; set; }
+
+        public const int NBMAXELEVES = 30;
+        public const int NBMAXMATIERES = 10;
+
+        public Classe(string nomClasse)
+        {
+            this.nomClasse = nomClasse;
+            eleves = new List<Eleve>();
+            matieres = new List<string>();
+            moyennesClasse = new List<float>();
+
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (eleves.Count < NBMAXELEVES)
+            {
+                eleves.Add(new Eleve(prenom, nom));
+            }
+            else
+            {
+                Console.WriteLine($"Le nombre d'élèves de cette classe dépasse {NBMAXELEVES}.");
+            }
+        }
+
+        public void ajouterMatiere(string nomMatiere)
+        {
+            if (matieres.Count < NBMAXMATIERES)
+            {
+                matieres.Add(nomMatiere);
+            }
+            else
+            {
+                Console.WriteLine($"Le nombre de matières de cette classe dépasse {NBMAXMATIERES}");
+            }
+        }
+
+        // Calcul de la moyenne de la classe par matière
+        public float moyenneMatiere(int numMatiere)
+        {
+            // Récupérer les moyennes des élèves dans la matière indiquée
+            var moyennesEleves = new List<float>();
+            foreach (Eleve eleve in eleves)
+            {
+                moyennesEleves.Add(eleve.moyenneMatiere(numMatiere));
+            }
+            return (float)Math.Truncate(moyennesEleves.Average() * 100) / 100;
+        }
+
+        // Calcul de la moyenne générale de la classe
+        public float moyenneGeneral()
+        {
+            // Ajouter à la collection des moyennes d'une classe la moyenne dans chaque matière
+            for (int i = 0; i < matieres.Count; i++)
+            {
+                moyennesClasse.Add(moyenneMatiere(i));
+            }
+            return (float)Math.Truncate(moyennesClasse.Average() * 100) / 100;
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    class Eleve
+    {
+        public string prenom { get; }
+        public string nom { get; }
+        public List<Note> notes { get; }
+        public List<float> moyennes { get; set; }
+
+        public const int NBMAXNOTES = 200;
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            notes = new List<Note>();
+            moyennes = new List<float>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            if (notes.Count <  NBMAXNOTES)
+            {
+                notes.Add(note);
+            }
+            else
+            {
+                Console.WriteLine($"Le nombre de notes dépasse {NBMAXNOTES}, l'élève est trop fatigué.");
+            }
+        }
+
+        // Calcul de la moyenne de l'élève par matière
+        public float moyenneMatiere(int numMatiere)
+        {
+            // Récupérer les notes de l'élève dans la matière indiquée
+            var notesMatiere = new List<Note>();
+            foreach (Note note in notes)
+            {
+                if (note.matiere == numMatiere)
+                {
+                    notesMatiere.Add(note);
+                }
+            }
+            return (float)Math.Truncate(notesMatiere.Average(n => n.note) * 100) / 100;
+        }
+
+        // Calcul de la moyenne générale d'un élève
+        public float moyenneGeneral()
+        {
+            // Récupérer le nombre de matières
+            int nombreMatieres = 0;
+            foreach (Note note in notes)
+            {
+                if (note.matiere == nombreMatieres)
+                {
+                    nombreMatieres++;
+                }
+            }
+            // Ajouter à la collection des moyennes d'un élève sa moyenne dans chaque matière
+            for (int i = 0; i < nombreMatieres; i++)
+            {
+                moyennes.Add(moyenneMatiere(i));
+            }
+            return (float)Math.Truncate(moyennes.Average() * 100) / 100;
+        }
+    }
+}
